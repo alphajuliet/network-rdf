20407cff53e4688da07e423101e9770036471dbb
AndrewJ <web@alphajuliet.com>
2012-10-26 08:08:29 +1100

    Refactored the SPARQL queries into a separate client class. Separated out different queries into their own source files.

--- examples/query.rb
+++ examples/query.rb
@@ -1,61 +1,13 @@
 #!/usr/bin/env ruby
 # Test SPARQL requests to 4store
 
-require 'rubygems'
-require 'rdf'
-require 'rest-client'
-require 'rexml/document'
-require 'sparql/client'
+$:.unshift File.join(File.dirname(__FILE__), "..", "src")
 
-module RDF
-	AJP = RDF::Vocabulary.new("http://alphajuliet.com/ns/person#")
-	AJO = RDF::Vocabulary.new("http://alphajuliet.com/ns/org#")
-	ORG = RDF::Vocabulary.new("http://www.w3.org/ns/org#")
-	GLDP = RDF::Vocabulary.new("http://www.w3.org/ns/people#")
-	V = RDF::Vocabulary.new("http://www.w3.org/2006/vcard/ns#")
-end
-
-# SPARQL query using Ruby client
-@sparql = SPARQL::Client.new('http://localhost:8000/sparql/')
-def query1
-	pattern = [:p, RDF::FOAF[:name], :n]
-	query = @sparql.select(:n).where(pattern).limit(10)
-	query.each_solution do |solution|
-		puts solution.n.to_s
-	end
-end
-
-
-# Queries using real SPARQL and XML
-class Sparql
-
-	def initialize(endpoint='http://localhost:8000/sparql/')
-		@endpoint = endpoint
-		@prefixes = "
-			PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-			PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-			PREFIX skos: <http://www.w3.org/2004/02/skos/core>
-			PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-			PREFIX gldp: <http://www.w3.org/ns/people#>
-			PREFIX org: <http://www.w3.org/ns/org#>
-			PREFIX v: <http://www.w3.org/2006/vcard/ns#>"
-	end
-	
-	def run_query(query, path, &block)
-		puts "POSTing SPARQL query to #{@endpoint}"
-		response = RestClient.post @endpoint, :query => (@prefixes << query)
-		puts "Response #{response.code}"
-		xml = REXML::Document.new(response.to_str)
-		REXML::XPath.each(xml, path, 'sparql' => 'http://www.w3.org/2005/sparql-results#').each do |entry|
-			yield(entry)
-		end
-	end	
-	
-end
-
+require 'sparql-client'
+require 'my_prefixes'
 
 # List the first 10 people that work for Oracle Australia and their mobile number
-query2 = '
+query = '
 SELECT ?name ?number WHERE {
   ?m a org:Membership ;
 		 org:organization [ rdfs:label "Oracle Australia" ] ;
@@ -69,27 +21,9 @@ SELECT ?name ?number WHERE {
 ORDER BY ?name
 LIMIT 10'
 
-# List people that don't have an email address
-query3 = '
-SELECT ?name WHERE {
-	?card a v:VCard ;
-        v:fn ?name .
-	OPTIONAL { 
-		?card v:email ?e.
-	}
-	FILTER (!bound(?e))
-}'
-
-query4 = '
-SELECT DISTINCT ?orgname WHERE {
-	?m org:organization [ rdfs:label ?orgname ] .
-}
-ORDER by ?orgname'
-
-s = Sparql.new
-s.run_query(query3, '//sparql:binding/sparql:literal') do |e|
+s = SparqlClient.new
+s.run_query(query, '//sparql:binding/sparql:literal') do |e|
 	puts e.text
 end
-
 
 # The End
---
+++ examples/query3.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+$:.unshift File.join(File.dirname(__FILE__), "..", "src")
+require 'sparql-client'
+require 'my_prefixes'
+
+# List people that don't have an email address
+query = '
+SELECT ?name WHERE {
+	?card a v:VCard ;
+        v:fn ?name .
+	OPTIONAL { 
+		?card v:email ?e.
+	}
+	FILTER (!bound(?e))
+}'
+
+s = SparqlClient.new
+s.run_query(query, '//sparql:binding/sparql:literal') do |e|
+	puts e.text
+end
+
+# The End
